06d62c3b05719dd97ad93af19f6f6e5368ec671c
lzkrishmom <lzkrishmom@gmail.com>
2023-07-01 23:01:02 -0400

    5th

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@
 //get the input data for calculation
 const Principle = document.getElementById('amount');
 const Rate = document.getElementById('interest');
-const Years = document.getElementById('time');
+const Time = document.getElementById('time');
 const timeChoice = document.getElementById("time_choice");
 const inputForm = document.querySelector('.input_form')
 const Clear = document.getElementById('clear');
@@ -14,38 +14,56 @@ const endBalance = document.getElementById("end_balance")
 const interestEarned = document.querySelectorAll(".interest_earned");
 const inputAmount = document.querySelectorAll('.input_amount');
 const inputInterest = document.getElementById("input_interest");
-const inputYear = document.getElementById("input_year");
+const inputTime = document.getElementById("input_time");
+const GetSchedule = document.getElementById('schedule');
 
 /*get inputs and clear*/
-
 inputForm.addEventListener('submit', getInput);
 
 function getInput(e) {
     e.preventDefault();
-    //calculate the time
-    let terms;
-
-    timeChoice.value === "month"? terms = terms = Math.round((Years.value / 12)*10)/10: terms = Years.value
-    //calculate interest_earned and end_balance
-    let principleInt;
-    
-    // reset end_balance to 0 otherwise end_balance gives NaN when form reset
-  isNaN(parseInt(Principle.value)) ? principleInt = 0 : principleInt = parseInt(Principle.value)
+    //calculate the terms of investment depending on month or year choose
+  let terms;
+    timeChoice.value === "month"? terms = Math.round((Time.value / 12)*100)/100: terms = Time.value
+    //convert principle.value and Rate.value to integer
+  let principleInt;
+  let rateInt;
+  isNaN(parseInt(Principle.value))
+    ? principleInt = 0
+    : principleInt = parseInt(Principle.value)
+  isNaN(parseInt(Rate.value))
+    ? (rateInt = 0)
+    : (rateInt= parseInt(Rate.value));
+  //calculate interest earned and end balance
+  let interestBank = [];
+  let termBank = [];
+  let endBalanceBank = [];
+  let interest_earned;
+  let end_balance;
   
-    const interest_earned = Math.round((principleInt * (Rate.value / 100) * terms)*100)/100;
-    const end_balance = principleInt+ interest_earned;
-    
+    for (let i = 1; i <= Time.value; i++) {
+      timeChoice.value === "month"
+        ? (interest_earned =
+            Math.round(((principleInt * (rateInt / 100) * i/12)) * 100) / 100)
+        : (interest_earned =
+            Math.round(principleInt * (rateInt / 100) * i * 100) / 100);
+      end_balance = principleInt + interest_earned;
+      termBank.push(i);
+      interestBank.push(interest_earned);
+      endBalanceBank.push(end_balance);
+  } 
     //display the results
     endBalance.innerHTML = end_balance;
     
     interestEarned.forEach((item) => item.innerHTML = interest_earned);
     inputAmount.forEach((item) => item.innerHTML = Principle.value);
     inputInterest.innerHTML = Rate.value;
-  inputYear.innerHTML = terms;
+    inputTime.innerHTML = terms;
   
   //call the charts
   getPiechart(principleInt, interest_earned);
-  getBargraph(principleInt, Rate.value, terms)
+  getBargraph(termBank, endBalanceBank, timeChoice.value)
+  getSchedule(termBank, endBalanceBank, interestBank, timeChoice.value)
 }
 Clear.addEventListener('click', clearData)
 
@@ -54,13 +72,13 @@ function clearData(){
     interestEarned.forEach((item) => (item.innerHTML = 0));
     inputAmount.forEach((item) => (item.innerHTML = 0));
     inputInterest.innerHTML = 0;
-    inputYear.innerHTML = 0;
-  document.querySelector("form").reset();
-
+    inputTime.innerHTML = 0;
+    document.querySelector("form").reset();
 }
 
 /**charts, graph, tables***/
-let pieChart = null; //make this a global variable so it can be accessible in multiple functions
+let pieChart = null; //initialize as global variable so it can be accessible in multiple functions
+let barGraph = null;
 function getPiechart(principleInt, interest_earned) {
   //data block
   const data = {
@@ -88,19 +106,16 @@ function getPiechart(principleInt, interest_earned) {
   }
   pieChart = new Chart(document.getElementById("pie_chart"), config);
 }
-function getBargraph(principleInt, rate, terms) {
-  //calculations
-   const interest_earned =
-     Math.round(principleInt * (rate / 100) * terms * 100) / 100;
-   const end_balance = principleInt + interest_earned;
+function getBargraph(termBank, endBalanceBank, timeChoice) {
   // Data block
   const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: termBank.map(item => item),
     datasets: [
       {
         label: "Ending Balance",
-        data: [12, 19, 3, 5, 2, 3],
+        data: endBalanceBank.map(item=>item),
         borderWidth: 1,
+        backgroundColor: "orange"
       },
     ],
   }
@@ -112,11 +127,57 @@ function getBargraph(principleInt, rate, terms) {
       scales: {
         y: {
           beginAtZero: true,
+          title: {
+            display: true,
+            text:"$ dollars"
+          }
         },
+        x: {
+          title: {
+            display: true,
+            text: timeChoice
+          }
+        }
       },
     },
   };
   
   // Render initiation block
-  const barGraph = new Chart(document.getElementById("bar_graph"), config);  
+  if (barGraph !== null) {
+    barGraph.destroy();
+  }
+  barGraph = new Chart(document.getElementById("bar_graph"), config);  
 }
+function getSchedule(termBank, endBalanceBank, interestBank, timeChoice) {
+  GetSchedule.innerHTML = `
+              <table id ="table">
+                  <tr>
+                    <th>Term (${timeChoice})</th>
+                    <th>Interest Earned</th>
+                    <th>End Balance</th>
+                  </tr>
+              </table>
+              ` ;
+  const table = document.getElementById('table');
+  for (let i = 0; i < termBank.length; i++){
+    let row = document.createElement('tr'); 
+    let dT = document.createElement('td'); //for term
+    let dIB = document.createElement('td');//for interest 
+    let dEB = document.createElement('td');//for balance
+
+    // append info to data cells
+    dT.append(termBank[i]);
+    dIB.append(interestBank[i]);
+    dEB.append(endBalanceBank[i]);
+
+    //append td to row
+    row.appendChild(dT);
+    row.appendChild(dIB);
+    row.appendChild(dEB);
+
+    //append rows to table
+    table.appendChild(row);
+
+
+  }
+ }
